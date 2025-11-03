0a7de4c2eabb124252439a76ec3c3ded6968a081
ANikolskaya <ANikolskaya@iikosoftware.com>
2025-11-03 20:51:48 +0500

    Сделала подсветку столбика на ховер (и убрала возможность менять размер поля)

--- src/components/board/board.tsx
+++ src/components/board/board.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type { GameBoardProps } from '../../interfaces/board.interface.ts';
 import './board.scss'
 
@@ -6,24 +6,29 @@ import './board.scss'
 export const GameBoard: React.FC<GameBoardProps> = ({
                                                         board,
                                                         onColumnClick,
-                                                        winningPositions = [],
-                                                        config
+                                                        winningPositions = []
                                                     }) => {
+    const [hoveredColumn, setHoveredColumn] = useState<number | null>(null);
+
     const isWinningPosition = (row: number, col: number) => {
         return winningPositions.some(pos => pos[0] === row && pos[1] === col);
     };
 
     return (
-        <div className="game-board" style={{
-            gridTemplateColumns: `repeat(${config.columns}, 60px)`,
-        }}>
+        <div className="game-board">
             {board.map((row, rowIndex) =>
                 row.map((cell, colIndex) => (
                     <div
-                        key={`${rowIndex}-${colIndex}`}
-                        className={`cell ${cell || 'cell__empty'} ${isWinningPosition(rowIndex, colIndex) ? 'cell__winning' : ''}`}
-                        onClick={() => onColumnClick(colIndex)}
-                    />
+                        className={`cell-wrapper ${hoveredColumn === colIndex ? 'column-hover' : ''}`}
+                        onMouseEnter={() => setHoveredColumn(colIndex)}
+                        onMouseLeave={() => setHoveredColumn(null)}
+                    >
+                        <div
+                            key={`${rowIndex}-${colIndex}`}
+                            className={`cell ${cell || 'cell__empty'} ${isWinningPosition(rowIndex, colIndex) ? 'cell__winning' : ''}`}
+                            onClick={() => onColumnClick(colIndex)}
+                        />
+                    </div>
                 ))
             )}
         </div>
